dda4390be3023d98740c3418048269171b1d340a
AdedayoJs <samfeolu@gmail.com>
2019-07-13 23:40:56 +0100

    Added Second Test To check for existence of data in database

--- __test__/app.test.ts
+++ __test__/app.test.ts
@@ -7,3 +7,15 @@ test('It should return all is well', async () => {
   expect(result.body).toEqual(msg);
   expect(result.status).toBe(200);
 });
+
+test('It should contain a specified object', async () => {
+  const contact: {} = {
+    firstname: 'adedayo',
+    lastname: 'adedunye',
+    phone: '08066069526',
+    id: 1,
+    isBlocked: false
+  };
+  const result = await request(app).get('/api/contacts');
+  expect(result.body).toContainEqual(contact);
+});
